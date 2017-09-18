81be36ba471eb11cdfaad6d1126227cf422f385c
guinus22800 <guinus22800@hotmail.fr>
2017-09-18 15:28:15 -0400

    aa

--- test_traitement_audio_2.py
+++ test_traitement_audio_2.py
@@ -1,33 +1,51 @@
-import os
+##############Import librairie
 import wave
-
 import pylab as pb
-import numpy as np
 
+
+#############Constante
 #Fréquence d'échantillonage = 2 * fc
-FE = 10000
+FE = 22050
 #NFFT echantillonnage - length of the windowing segments
 NFFT = 1024
 
-def graph_spectrogram(wav_file):
-    sound_info, frame_rate = get_wav_info(wav_file)
+#############Fonction get_Info du fichier audio
+#Return les info de l'audio et sa duree
+def get_wav_info(wav_file):
+    #Ouverture du fichier en lecture
+    wav = wave.open(wav_file, 'r')
+    frames = wav.readframes(-1)
+    sound_info = pb.fromstring(frames, 'Int64')
+    frame = wav.getnframes()
+    print(frame)
+    rate = wav.getframerate()
+    #Calcul duree de l audio
+    duration = frame/float(rate)
+    wav.close()
+    #Return
+    return sound_info, duration
+
+#############Fonction enregistrement du spectrogramme
+def graph_spectrogram(wav_file, name):
+    #Recupere les infos de  audio
+    sound_info, duration = get_wav_info(wav_file)
     pb.figure(num=None, figsize=(19, 12))
     pb.subplot(111)
     pb.title('spectrogram of %r' % wav_file)
-    n=2100
+    #Nombre dechantillons
+    n=int(duration*FE)
+    print(n)
+    n=int(44100/3)
+    #Desine le spectrogramme d une partie de l audio
     pb.specgram(sound_info[n:n+256*256], NFFT=NFFT, Fs=FE, noverlap=1000, cmap='jet')
-    pylab.savefig('spectrogram_parole.png')
-    #pb.savefig('spectrogram_musique.png')
+    #Enregistrement du spectrogramme
+    pb.savefig(name + '.png')
 
-def get_wav_info(wav_file):
-    wav = wave.open(wav_file, 'r')
-    frames = wav.readframes(-1)
-    sound_info = pylab.fromstring(frames, 'Int64')
-    frame_rate = wav.getframerate()
-    wav.close()
-    return sound_info, frame_rate
 
-if __name__ == '__main__': # Main function
-    wav_file = 'ben_bravo.wav' # Filename of the wav file
-    graph_spectrogram(wav_file)
-    
+
+#############Fonction main
+if __name__ == '__main__':
+    #Boucle de traitement des audios
+    wav_file_parole = 'parole2.wav' # Filename of the wav file
+    wav_file_musique = 'musique.wav' # Filename of the wav file
+    graph_spectrogram(wav_file_parole, 'spectrogram_parole')
