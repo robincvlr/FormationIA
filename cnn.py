3f24dce5c843e1b9f48c0939b165ba5a5d234a9d
Robin <robin.cavalieri@polytech-lille.net>
2017-09-20 15:11:01 -0600

    nouvelle version de l'apprentissage par image

--- cnn.py
+++ cnn.py
@@ -1,47 +1,42 @@
-# Convolutional Neural Network
+# Convolutional Neural Network 
 
-# Building the CNN
+# Part 1 - Building the CNN 
 
-# Importing the keras librairies and packages
-from keras.models import Sequential # initilisation du reseau neurones (couches/entree/sortie)
+from keras.models import Sequential # Initialiser les couches du réseau de neurones 
 from keras.layers import Convolution2D # Convolution 
-from keras.layers import MaxPooling2D # Pooling step
-from keras.layers import Flatten # Flatting 
-from keras.layers import Dense # Connecter tous les neurones 
+from keras.layers import MaxPooling2D # Pooling step 
+from keras.layers import Flatten # Convert Pool into vectors (connexions) 
+from keras.layers import Dense # Add the fully connected layers 
 
-# Initialise le CNN
+# initialising the CNN 
 classifier = Sequential()
 
-# Convolution Step 1
-# 32 --> nombre filters : nombre matrice detection ?
-# 3, 3 --> kernel_size
-# input_shape --> 64, 64 --> plage de couleur et 3 channel car en couleur 1 si noir et blanc
-# activation --> fonction d activation
-classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation= 'relu'))
+# Step 1  - convolution 
+# Paramètres de la matrice de détection, couleurs, taille, etc 
+classifier.add(Convolution2D(32, 3, 3, input_shape=(64, 64, 3), activation='relu'))
 
-# Pooling step
-# pool_size= (2,2) --> reduit la matrice obtenu apres convolution sans perdre d info
-classifier.add(MaxPooling2D(pool_size= (2,2)))
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2))) 
 
-# Flattening step
-# convertir la matrice de Pooling en verteur d entrée a une seule dimension
-# Kares va gerer : pas besoin d entree
+# Step 1  - convolution 
+# Paramètres de la matrice de détection, couleurs, taille, etc 
+classifier.add(Convolution2D(32, 3, 3, activation='relu'))
+
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2))) 
+ 
+# Step 3 - Flattening 
 classifier.add(Flatten())
 
-# Full connection : faire un reseau de neurone ANN 
-# Make a hiiden layer
-classifier.add(Dense(output_dim = 128, activation= 'relu'))
-# Make the output layer
-classifier.add(Dense(output_dim = 1, activation= 'sigmoid'))
+# Step 4 - Full connection
+classifier.add(Dense(output_dim = 128, activation = 'relu')) 
+classifier.add(Dense(output_dim = 1, activation = 'sigmoid')) 
 
-# Compiling the CNN
-classifier.compile(optimizer='adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+# Compilation of the CNN 
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-# Fitting the CNN to the images
-# Image augmentation process
-# Creer un jeux de donner avec plus d image (rotation/zoom/...)
+# Part 2 - Fitting the CNN to the images 
 from keras.preprocessing.image import ImageDataGenerator
-
 train_datagen = ImageDataGenerator(
         rescale=1./255,
         shear_range=0.2,
@@ -50,24 +45,72 @@ train_datagen = ImageDataGenerator(
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
-# target_size --> dimension de l image definie plus haut
-# class_mode --> binary car uniquement deux valeurs
-training_set = train_datagen.flow_from_directory(
+train_generator = train_datagen.flow_from_directory(
         'dataset/training_set',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
 
-test_set = test_datagen.flow_from_directory(
+validation_generator = test_datagen.flow_from_directory(
         'dataset/test_set',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
 
 classifier.fit_generator(
-        training_set,
-        steps_per_epoch=2000,
-        epochs=25,
-        validation_data=test_set,
+        train_generator,
+        steps_per_epoch=200,
+        epochs=5,
+        validation_data=validation_generator,
         validation_steps=200)
+
+#Save the learning 
+#classifier.save('sauvegarde.tfl')
+
+#serialise model to json
+classifier_json = classifier.to_json()
+with open('model.json', "w") as json_file :
+    json_file.write(classifier_json)
+    
+# serialize weights to HDF5
+classifier.save_weights("model.h5")
+print("Classifier save on disk")
+
+# Load json and create model
+from keras.models import model_from_json
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+
+#load weights into new model
+loaded_model.load_weights('model.h5')
+print("Loaded model from disk")
+
+#Making a prediction 
+
+import numpy as np
+from keras.preprocessing import image
+
+# Load the image with keras 
+# arg : path / target_size same as the test_set
+test_image = image.load_img('dataset/single_prediction/ok.jpg', target_size=(64, 64))
+# passer l'image en tableau
+test_image = image.img_to_array(test_image)
+
+#test method
+#mauvais format, il faut ajouter une dimension au test 
+test_image = np.expand_dims(test_image, axis = 0)
+classifier.predict(test_image)
+
+#Correspondance 
+result  = classifier.predict(test_image)
+
+# 1 est un chat ou un chien ???
+train_generator.class_indices
+
+if result [0][0] == 1: 
+    prediction='dog'
+else:
+    predition='cat'
 
