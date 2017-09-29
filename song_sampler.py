e0e766df6b8b710ec6788f42d3d925d48455f8ee
robincvlr <robin.cavalieri@polytech-lille.net>
2017-09-29 15:17:39 -0400

    mdif song sampler

--- song_sampler.py
+++ song_sampler.py
@@ -8,29 +8,32 @@ Created on Mon Sep 25 15:01:56 2017
 #Le but de ce fichier est de découper tous les sons en extraits de 4s
 
 import wave 
+import os
 
 ############# TRAITEMENT DES AUDIOS
-def traitement(wav_file):
+def traitement(wav_name):
      sample_freq = 0
      sample_width = 0 
+     #detection du fichier
+     wav_file = 'sons_musique/'+ wav_name 
+     print(wav_file)
      # ouverture du fichier
-     wave_write = wave.openfp(wav_file, 'r')
+     wave_read = wave.openfp(wav_file, 'rb')
+     print('1')
      # valeur de l'echantillonn. en bits
-     sample_width = wave_write.getsampwidth()
+     sample_width = wave_read.getsampwidth()
      print ("sample width : %d" %sample_width)
      # Fréquence d'échantillonnage
-     sample_freq = wave_write.getframerate() 
+     sample_freq = wave_read.getframerate() 
      print ("sample frequency : %d" %sample_freq) 
-     wave.close()
+     wave_read.close()
      
      
 ############# MAIN
 if __name__ == '__main__':
     #Boucle de traitement des audios
-    i = 0
-    while i < 2 :
-        i = i+1
-        wav_file = 'sons_musique/Musique (' + str(i) + ').wav' 
-        traitement(wav_file)
+    for element in os.listdir('sons_musique/' ):
+        if element.endswith('.wav'):
+            traitement(element)
      
     
