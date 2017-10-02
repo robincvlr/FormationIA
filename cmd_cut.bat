86699ba64077901cb99447289a81295f050f48d2
robincvlr <robin.cavalieri@polytech-lille.net>
2017-10-02 16:36:56 -0400

    test cut batch failed

--- cmd_cut.bat
+++ cmd_cut.bat
@@ -1,5 +1,18 @@
+::L'objectif de ce script est de découper les échantillons afin qu'ils
+::aient une durée de 3s chacun - OBJ : apprentisage plus précis - limiter les erreurs 
 
-for %%X in (*.wav) do (
-echo Traitement de %%X...
-ffmpeg -t 30 -ss 00:00:00.000 -i %%X -acodec copy %%X
-)
+::découpage des fichiers musicaux
+set i=0
+for %%X in (sons_musique/*.wav) do (
+echo Traitement de %%X... 
+echo valeur de i %i%...
+ffmpeg -ss 00:00:00.001 -i "sons_musique/%%X" -t 00:00:03.001 "sons_musique/extrait%i%.wav"
+set /a i+=1)
+
+
+::découpage des fichiers vocaux
+::set /A i=0
+::for %%Y in (sons_parole/*.wav) do (
+::echo Traitement de %%Y...
+::ffmpeg -ss 00:00:00.001 -t 00:00:03.001 -i "sons_parole/%%Y" -t 00:00:03.001 "sons_parole/extrait%i%.wav"
+::set /a i+=1)
